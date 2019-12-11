d95ea1a684c3c15b10f08c0b5f478e9d56e9ba0a
Ahmed <ahmed.max7586@gmail.com>
2019-12-11 09:52:42 -0500

    ready for deploy

--- wp-content/themes/astra/template-parts/404/404-layout.php
+++ wp-content/themes/astra/template-parts/404/404-layout.php
@@ -6,5 +6,5 @@
 <tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Error: Call to undefined function astra_the_title() in /Users/ahmedmannan/Local Sites/jaguarlimo/app/public/wp-content/themes/astra/template-parts/404/404-layout.php on line <i>15</i></th></tr>
 <tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
 <tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0005</td><td bgcolor='#eeeeec' align='right'>405832</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/Users/ahmedmannan/Local Sites/jaguarlimo/app/public/wp-content/themes/astra/template-parts/404/404-layout.php' bgcolor='#eeeeec'>.../404-layout.php<b>:</b>0</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0001</td><td bgcolor='#eeeeec' align='right'>405032</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/Users/ahmedmannan/Local Sites/jaguarlimo/app/public/wp-content/themes/astra/template-parts/404/404-layout.php' bgcolor='#eeeeec'>.../404-layout.php<b>:</b>0</td></tr>
 </table></font>
